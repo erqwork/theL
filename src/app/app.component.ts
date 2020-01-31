ae6454f4d31cf085be9ef0cf35c605cd2c71f31d
erqwork <erqwork@gmail.com>
2020-01-31 11:50:03 -0600

    Pushing to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,10 @@ import { third } from '../assets/Updating/upThirdSet';
 import { fourth } from '../assets/Updating/upFourthSet';
 import { fifth } from '../assets/Updating/upFifthSet';
 import { sixth } from '../assets/Updating/upSixthSet';
+import * as FD from '../assets/Differences/firstDiffs.json';
+import * as SD from '../assets/Differences/secondDiffs.json';
+import * as TD from '../assets/Differences/thirdDiffs.json';
+import * as FthD from '../assets/Differences/fourthDiffs.json';
 
 import { numbs } from '../assets/numbs';
 
@@ -34,6 +38,11 @@ export class AppComponent implements OnInit{
   public fourthNums = [];
   public fifthNums = [];
   public sixthNums = [];
+
+  // Options
+  public highsLowsModesOpts: any = {};
+  public occsOpts: any = {};
+  public diffOpts: any = {};
 
   // Smallest numbers in each set
   public firstSmallest: number;
@@ -81,6 +90,7 @@ export class AppComponent implements OnInit{
   public calcdPercs = [];
   public allNumbs: any;
   
+  public seventyArr: number[] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70]; 
   public goalArr: number[] = [16,18,28,33,67,14];
   public randodArr:any = [];
   public strungDis: any;
@@ -140,6 +150,162 @@ export class AppComponent implements OnInit{
     // Find and assign - Occurences
     this.sixthOccs = this.findOccurences(sixth);
     this.stringOcc = JSON.stringify(this.sixthOccs);  
+
+    // Assign options
+    this.highsLowsModesOpts = {
+      chart: {
+        type: 'column'
+      },
+      title: {
+        text: 'Highest, Lowest, and Modes'
+      },
+      credits: {
+        enabled: false
+      },      
+      xAxis: {
+        categories: ['[0]', '[1]', '[2]', '[3]', '[4]', '[5]']
+      },
+      yAxis: {
+        min: 0,
+        max: 70,
+        title: {
+          text: 'Possible numbers'
+        }
+      },
+      plotOptions: {
+        column: {
+          grouping: false,
+          shadow: false,
+          borderWidth: 0
+        }
+      },
+      series: [        
+        {
+          name: 'highest',
+          data: [40, 59, 66, 68, 70, 25],
+          pointPadding: 0.1,
+          color: 'rgba(255,128,85,1)'
+        }
+        ,{
+          name: 'mode',
+          data: [1,17,31,42,70,10],
+          pointPadding: 0.2,
+          color: 'rgba(128,255,85,1)'
+        }
+        ,{
+          name: 'lowest',
+          data: [1, 2, 4, 8, 18, 1],
+          pointPadding: 0.3,
+          color: 'rgba(128,85,255,1)'
+        }
+        
+      ]
+    }   
+
+    this.occsOpts = {
+      chart: {
+        type: 'column'
+      },
+      title: {
+        text: 'Occurences'
+      },
+      credits: {
+        enabled: false
+      },
+      xAxis: {
+        categories: this.seventyArr
+      },
+      yAxis: {
+        min: 0,
+        max: 20,
+        title: {
+          text: 'Numbers of time each number appears'
+        }
+      },
+      plotOptions: {
+        column: {
+          grouping: false,
+          shadow: false,
+          borderWidth: 0
+        }
+      },
+      series: [              
+        {
+          name: 'first digit',
+          data: [20, 14, 13, 12, 7, 5, 8, 11, 5, 13, 7, 5, 4, 9, 4, 6, 5, 2, 1, 3, 3, 1, 1, 3, 2, 0, 1, 5, 3, 0, 1, 0, 2, 2, 0, 0, 0, 0, 1, 1],
+          pointPadding: 0.2,
+          color: '#393D2C'
+        }
+        ,{
+          name: 'second digit',
+          data: [0, 2, 2, 2, 2, 4, 3, 3, 2, 6, 4, 5, 5, 7, 5, 8, 9, 3, 3, 3, 1, 9, 4, 8, 5, 5, 0, 8, 6, 6, 3, 4, 5, 6, 2, 3, 5, 6, 0, 1, 3, 2, 1, 1, 2, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+          pointPadding: 0.22,
+          color: '#2DC1B7'
+        }
+        ,{
+          name: 'third digit',
+          data: [0,0,0,1,1,0,0,0,0,1,2,1,2,2,3,1,0,3,1,3,4,4,5,2,4,4,3,5,4,7,10,6,3,5,2,3,6,3,11,7,5,5,3,4,2,4,5,3,2,0,4,2,4,1,3,1,2,4,0,1,3,1,1,0,0,1],
+          pointPadding: 0.24,
+          color: '#9FDFC7'
+        }        
+        ,{
+          name: 'fourth digit',
+          data: [0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,1,2,0,1,2,0,3,1,0,2,1,1,1,2,4,5,2,2,1,2,7,1,3,2,10,8,5,1,7,4,9,7,2,1,2,4,8,1,8,3,2,5,7,6,10,2,6,6,3,2,2],
+          pointPadding: 0.26,
+          color: '#CE986A'
+        }
+        , {
+          name: 'fifth digit',
+          data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,1,0,0,0,1,1,1,2,2,3,2,2,2,1,4,4,4,1,4,2,1,3,3,8,8,10,6,6,11,7,10,5,10,11,13,10,17],
+          pointPadding: 0.28,
+          color: '#E14A38'
+        }
+      ]
+    }   
+
+    this.diffOpts = {
+      chart: {
+        type: 'area'
+      },
+      title: {
+        text: 'Differences'
+      },
+      credits: {
+        enabled: false
+      },     
+      plotOptions: {        
+        series: {
+          label: {
+              connectorAllowed: false
+          }
+        }
+      },
+      series: [        
+        {
+          name: 'first & second digits',
+          data: FD.differences,
+          color: 'rgba(255,85,85,1)'
+        },
+        {
+          name: 'second & third digits',
+          data: SD.differences,
+          color: 'rgba(255,128,85,1)'
+        },
+        {
+          name: 'third & fourth digits',
+          data: TD.differences,
+          lineColor: 'rgba(128,255,85,1)',
+          color: 'rgba(128,255,85,0.5)',
+        },
+        {
+          name: 'fourth & fifth digits',
+          data: FthD.differences,
+          lineColor: 'rgba(85,128,255,1)',
+          color: 'rgba(85,128,255,0.5)'
+        },
+        
+      ]
+    }
 
   }
 
@@ -284,7 +450,7 @@ export class AppComponent implements OnInit{
     let randoStrung: string; 
     let iter = 1;
     let x = [16, 18, 28, 33, 67, 14];
-    let sx = JSON.stringify(x);
+    let sx = x.toString();
     // do {
     //   newRando = this.createIt();
     //   randoStrung = JSON.stringify(newRando);
@@ -295,7 +461,7 @@ export class AppComponent implements OnInit{
     //   }
     // } while (strungGoal !== randoStrung);
     iter++;
-    this.randodArr = JSON.stringify(this.createIt());
+    this.randodArr = this.createIt().toString();
     this.strungDis = strungGoal;
     console.log('final(?) iter', iter);    
     console.log('outside of while', this.randodArr);
@@ -303,7 +469,5 @@ export class AppComponent implements OnInit{
   
   getDiff(arr: any[], b, a): number {
     return arr[b] - arr[a];
-  }
-
-  
+  }  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GraphBoxComponent } from './components/graph-box/graph-box.component';
 
-import * as Highcharts from 'highcharts';
-import * as more from 'highcharts/highcharts-more.src';
-import * as exporting from 'highcharts/modules/exporting.src';
 import { HighChartComponent } from './components/high-chart/high-chart.component';
-import { OccGraphComponent } from './components/occ-graph/occ-graph.component';
-import { DiffGraphComponent } from './components/diff-graph/diff-graph.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GraphBoxComponent,
     HighChartComponent,
-    OccGraphComponent,
-    DiffGraphComponent
   ],
   imports: [
     BrowserModule,
